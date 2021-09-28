10fbcd42216a7a607ab79dec8fefc4760a143e5e
Noella Hirwa <noellahirwa@Noellas-MacBook-Pro.local>
2021-09-29 00:18:12 +0200

    commiting some changes in js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const heading = document.querySelector("logo-heading");
+
+function hover() {
+  heading.style.color = "blue";
+}
+
+heading.addEventListener("mouseover", hover);
